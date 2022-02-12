c8bc2a84d7fa8b387d50af258430a6392ec52618
the-chat <thechatllc@gmail.com>
2022-02-12 12:54:17 +0500

    tsc

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
-import { OptionsObject, SnackbarKey } from "notistack";
+import { OptionsObject, SnackbarKey, SnackbarMessage } from "notistack";
 import { FC } from "react";
 declare const useAlert: () => {
-    enqueueSnackbar: (message: string, options?: OptionsObject | undefined) => SnackbarKey;
+    enqueueSnackbar: (message: SnackbarMessage, options?: OptionsObject | undefined) => SnackbarKey;
     closeSnackbar: (key?: SnackbarKey | undefined) => void;
 };
 declare const AlertProvider: FC;
