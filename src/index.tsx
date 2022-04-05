0b6f83ce9383c97235d42468747bb2dcac867e85
the-chat <thechatllc@gmail.com>
2022-04-05 17:53:19 +0500

    index

--- src/index.tsx
+++ src/index.tsx
@@ -1,122 +1,13 @@
-import {
-  OptionsObject,
-  VariantType,
-  SnackbarKey,
-  SnackbarProvider,
-  SnackbarMessage,
-  useSnackbar,
-} from "notistack"
-import { Box, IconButton, Collapse, Typography, Theme } from "@mui/material"
-import { makeStyles, useTheme } from "@mui/styles"
-import { Close, Error, Warning, Info, CheckCircle } from "@mui/icons-material"
-import React, { FC } from "react"
-
-const useConfig = (variant: VariantType) => {
-  const theme = useTheme<Theme>()
-
-  switch (variant) {
-    case "default":
-      return {
-        colors: {
-          color: theme.palette.getContrastText(
-            theme.palette.getContrastText(theme.palette.background.default)
-          ),
-          backgroundColor: theme.palette.getContrastText(
-            theme.palette.background.default
-          ),
-        },
-        Icon: () => null,
-      }
-    case "error":
-      return {
-        colors: {
-          color: "error.contrastText",
-          backgroundColor: "error.main",
-        },
-        Icon: Error,
-      }
-    case "info":
-      return {
-        colors: {
-          color: "info.contrastText",
-          backgroundColor: "info.main",
-        },
-        Icon: Info,
-      }
-    case "success":
-      return {
-        colors: {
-          color: "success.contrastText",
-          backgroundColor: "success.main",
-        },
-        Icon: CheckCircle,
-      }
-    case "warning":
-      return {
-        colors: {
-          color: "warning.contrastText",
-          backgroundColor: "warning.main",
-        },
-        Icon: Warning,
-      }
-  }
-}
-
-const useAlert = () => {
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar()
-
-  // own variable for linting
-  const useContent = (
-    key: SnackbarKey,
-    { message, variant }: { message: string; variant: VariantType }
-  ) => {
-    const { colors, Icon } = useConfig(variant)
-
-    // todo: to config
-    const p = 1
-
-    return (
-      // todo?: add "swipe to close" feature
-      <Box
-        sx={{
-          ...colors,
-          wordBreak: "break-word",
-          display: "flex",
-          alignItems: "center",
-          p,
-        }}
-        key={key}
-      >
-        <Icon sx={{ mr: p }} />
-        <Typography variant="body2" sx={{ mr: "auto" }}>
-          {message}
-        </Typography>
-        <IconButton onClick={() => closeSnackbar(key)} color="inherit">
-          <Close />
-        </IconButton>
-      </Box>
-    )
-  }
-
-  return {
-    enqueueSnackbar: (message: SnackbarMessage, options?: OptionsObject) => {
-      return enqueueSnackbar(
-        {
-          message,
-          variant: options?.variant || "default",
-        },
-        {
-          content: useContent,
-          ...options,
-        }
-      )
-    },
-    closeSnackbar,
-  }
-}
+import { SnackbarProvider, ProviderContext } from "notistack"
+import { makeStyles } from "@mui/styles"
+import genContext, { ProviderWrapperProps } from "@the-chat/gen-context"
+import { PropsWithChildren } from "react"
+import NotificiactionInterface from "./NotificiactionInterface"
+import { Collapse } from "@mui/material"
+import useCustomSnackbar from "./useCustomSnackbar"
 
 const useStyles = makeStyles({
-  snackbarRoot: {
+  containerRoot: {
     left: "0 !important",
     bottom: "0 !important",
     "& > *": {
@@ -130,21 +21,35 @@ const useStyles = makeStyles({
   },
 })
 
-const AlertProvider: FC = ({ children }) => {
-  const { snackbarRoot } = useStyles()
+type RealProviderWrapperProps = PropsWithChildren<
+  Pick<ProviderWrapperProps<ProviderContext | undefined>, "RealProvider">
+>
 
-  return (
-    <SnackbarProvider
-      TransitionComponent={Collapse}
-      classes={{
-        containerRoot: snackbarRoot,
-      }}
-      maxSnack={1}
-    >
-      {children}
-    </SnackbarProvider>
-  )
-}
+const RealProviderWrapper = ({
+  children,
+  RealProvider,
+}: RealProviderWrapperProps) => (
+  <RealProvider value={useCustomSnackbar()}>{children}</RealProvider>
+)
+
+const [useAlert, AlertProvider] = genContext<ProviderContext>(
+  ({ RealProvider, children }) => {
+    const classes = useStyles()
+
+    return (
+      <SnackbarProvider
+        TransitionComponent={Collapse}
+        content={NotificiactionInterface}
+        classes={classes}
+        maxSnack={1}
+      >
+        <RealProviderWrapper RealProvider={RealProvider}>
+          {children}
+        </RealProviderWrapper>
+      </SnackbarProvider>
+    )
+  }
+)
 
 export { AlertProvider }
 export default useAlert
